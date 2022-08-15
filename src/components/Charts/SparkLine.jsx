030c3acadffcaa024375507c6269b3294085741d
Kweli hall <kweli.hall123@gmail.com>
2022-08-15 14:44:40 -0500

    sixth commit implamented the first chat to my web app.

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
 
 const SparkLine = ({ id, height, width, color, data, type, currentColor}) => {
+
   return (
     <SparklineComponent
       id={id}
@@ -13,11 +14,14 @@ const SparkLine = ({ id, height, width, color, data, type, currentColor}) => {
       border={{ color: currentColor, width: 2}}
       dataSource={data}
       xName="x"
-      yName="y"
+      yName="yval"
       type={type}
       tooltipSettings={{
         visible: true,
-        format: '${x} : data ${y}'
+        format: '${x}: data ${yval}',
+        trackLineSettings: {
+          visible: true
+        }
       }}
     >
       <Inject services={[SparklineTooltip]} />
