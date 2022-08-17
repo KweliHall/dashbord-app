d5333bc67728d7a2a00eae889523134eda2d2f70
Kweli hall <kweli.hall123@gmail.com>
2022-08-17 18:01:57 -0500

    ninth commit, finished orders page.

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMEnu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
 
 
-import { orderData, contextMenuItems, OrdersGrid, ordersGrid} from '../data/dummy';
+import { ordersData, contextMenuItems, OrdersGrid, ordersGrid} from '../data/dummy';
 import { Header } from '../components';
 
 const Orders = () => {
@@ -10,12 +10,18 @@ const Orders = () => {
     <div className="m-2 md:m-10 p-2 md:p-10 
     bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
-      <GridComponent>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
         <ColumnsDirective>
         {ordersGrid.map((item, index) => (
           <ColumnDirective key={index} {...item} />
         ))}
         </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport ]} />
       </GridComponent>
     </div>
   )
